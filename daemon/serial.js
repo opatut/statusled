99fda0b1429f17566c79b422004bdcefea19f610
Paul Bienkowski <pb@opatut.de>
2018-05-02 07:45:40 +0200

    Watch LED device file and re-set when it appears

--- daemon/serial.js
+++ daemon/serial.js
@@ -1,18 +1,36 @@
-const fs = require('fs');
-const shell = require('shelljs');
+const fs = require('fs')
+const shell = require('shelljs')
 
 const {wrapRetry} = require('./utils')
 
-const {ALL_STATUSES} = require('./constants');
+const {ALL_STATUSES} = require('./constants')
 
-module.exports = wrapRetry(async status => {
-  const PORT = '/dev/ttyACM0'
+const DEVICE_PATH = '/dev/ttyACM0'
+
+let lastStatus
+
+fs.watchFile(
+  DEVICE_PATH,
+  {
+    persistent: false,
+    interval: 2000,
+  },
+  function(stat) {
+    // ino: inode number. If non-zero, the file exists, and we can write there
+    if (stat.ino && lastStatus) {
+      write(lastStatus)
+    }
+  }
+)
+
+const write = (module.exports = wrapRetry(async status => {
+  lastStatus = status
   // make sure file exists
-  fs.statSync(PORT)
+  fs.statSync(DEVICE_PATH)
 
   const stty = [
     'stty -F',
-    PORT,
+    DEVICE_PATH,
     '9600 -parenb -parodd cs8 -hupcl -cstopb cread clocal -crtscts',
     '-iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl -onlcr -onocr -onlret -ofill -ofdel',
     'nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon -iexten -echo -echoe -echok -echonl -noflsh -xcase',
@@ -21,7 +39,7 @@ module.exports = wrapRetry(async status => {
   shell.exec(stty)
 
   const code = ALL_STATUSES.indexOf(status)
-  shell.exec(`echo -en '\\x0${code}' > ${PORT}`)
+  shell.exec(`echo -en '\\x0${code}' > ${DEVICE_PATH}`)
 
   console.log('set status LED to ' + status)
-}, 10000)
+}, 10000))
